fd64af8cc68aad88022be49bf58e66b17d9708dc
SquidDre <deandrebailey@Outlook.com>
2026-04-01 19:34:58 -0400

    finished login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Services from './pages/signup/services'
 import Favorites from './pages/signup/favorites'
 import Home from './pages/splash'
 import Create from './pages/signup/create'
+import Login from './pages/login'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/create" element={<Create />} />
         <Route path="/services" element={<Services />} />
         <Route path="/favorites" element={<Favorites />} />
